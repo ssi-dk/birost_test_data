9e7d39f2a5f16b9831bc04f4fe53755149f5bd65
Kim Ng <kimleeng@gmail.com>
2020-08-07 12:17:08 +0200

    Adjust them from local to test use

---
+++ per_sample.sh
@@ -0,0 +1,13 @@
+
+# Per sample script example
+echo "Running $sample.name from $run.name";
+BIFROST_RAW_DATA_MNT="/bifrost_test_data/data/";
+
+mkdir $sample.name;
+cd $sample.name;
+
+# Currently runs assuming you're in directory
+docker run --mount type=bind,source=$BIFROST_RAW_DATA_MNT,target=$BIFROST_RAW_DATA_MNT ssidk/bifrost-min_read_check:2.0.7 -id $sample._id
+docker run --mount type=bind,source=$BIFROST_RAW_DATA_MNT,target=$BIFROST_RAW_DATA_MNT ssidk/bifrost-whats_my_species:2.0.7 -id $sample._id
+
+cd ..;
---
+++ read_data/download_S1.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+wget -O S1_R1.fastq.gz "ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR430/ERR4301030/S1.R1.fastq.gz"
+wget -O S1_R2.fastq.gz "ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR430/ERR4301030/S1.R2.fastq.gz"
