262d75f3c544890148f31ada8974bb61d2726a8e
Kim Ng <kimleeng@gmail.com>
2020-09-03 14:29:31 +0200

    debug

--- per_sample.sh
+++ per_sample.sh
@@ -1,13 +1,11 @@
-#/bin/bash
 # Per sample script example
 echo "Running $sample.name from $run.name";
-BIFROST_RAW_DATA_MNT="/bifrost_test_data/data/";
 
 mkdir $sample.name;
 cd $sample.name;
 
 # Currently runs assuming you're in directory
-docker run --mount type=bind,source=$BIFROST_RAW_DATA_MNT,target=$BIFROST_RAW_DATA_MNT ssidk/bifrost-min_read_check:2.0.7 -id $sample._id
-docker run --mount type=bind,source=$BIFROST_RAW_DATA_MNT,target=$BIFROST_RAW_DATA_MNT ssidk/bifrost-whats_my_species:2.0.7 -id $sample._id
+docker run --mount type=bind,source=$BIFROST_DIR,target=$BIFROST_DIR ssidk/bifrost_min_read_check:latest -id $sample._id
+docker run --mount type=bind,source=$BIFROST_DIR,target=$BIFROST_DIR ssidk/bifrost_whats_my_species:latest -id $sample._id
 
 cd ..;
