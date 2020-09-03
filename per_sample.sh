74b77c72d528c4ae1d65ddb6bf284d06145f5b28
Kim Ng <kimleeng@gmail.com>
2020-09-03 15:05:04 +0200

    forgot to add env vars

--- per_sample.sh
+++ per_sample.sh
@@ -5,7 +5,7 @@ mkdir $sample.name;
 cd $sample.name;
 
 # Currently runs assuming you're in directory
-docker run --mount type=bind,source=$BIFROST_DIR,target=$BIFROST_DIR ssidk/bifrost_min_read_check:latest -id $sample._id
-docker run --mount type=bind,source=$BIFROST_DIR,target=$BIFROST_DIR ssidk/bifrost_whats_my_species:latest -id $sample._id
+docker run --env BIFROST_DB_KEY --mount type=bind,source=$BIFROST_DIR,target=$BIFROST_DIR ssidk/bifrost_min_read_check:latest -id $sample._id
+docker run --env BIFROST_DB_KEY --mount type=bind,source=$BIFROST_DIR,target=$BIFROST_DIR ssidk/bifrost_whats_my_species:latest -id $sample._id
 
 cd ..;
