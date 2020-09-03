a5af850b8bcd00cf8566399f3bc55c715b9bd06f
Kim Ng <kimleeng@gmail.com>
2020-09-03 13:47:34 +0200

    fix for download

--- read_data/download_S1.sh
+++ read_data/download_S1.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-[ ! -e "S1_R1.fastq.gz"] && wget -O S1_R1.fastq.gz "ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR430/ERR4301030/S1.R1.fastq.gz"
-[ ! -e "S1_R2.fastq.gz"] && wget -O S1_R2.fastq.gz "ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR430/ERR4301030/S1.R2.fastq.gz"
+[ ! -e "S1_R1.fastq.gz" ] && wget -O S1_R1.fastq.gz "ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR430/ERR4301030/S1.R1.fastq.gz"
+[ ! -e "S1_R2.fastq.gz" ] && wget -O S1_R2.fastq.gz "ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR430/ERR4301030/S1.R2.fastq.gz"
