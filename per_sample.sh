31fbe1d0cb6167bcd87957ae1d1a13aed41156e1
Kim Ng <kimleeng@gmail.com>
2020-09-16 13:40:34 +0200

    make it work for other components, leaving cge out temp as I haven't adjusted to new output

--- per_sample.sh
+++ per_sample.sh
@@ -4,8 +4,10 @@ echo "Running $sample.name from $run.name";
 mkdir $sample.name;
 cd $sample.name;
 
-# Currently runs assuming you're in directory
-docker run -w $BIFROST_DIR --env BIFROST_DB_KEY --mount type=bind,source=$BIFROST_DIR,target=$BIFROST_DIR ssidk/bifrost_min_read_check:latest -id $sample._id
-docker run -w $BIFROST_DIR --env BIFROST_DB_KEY --mount type=bind,source=$BIFROST_DIR,target=$BIFROST_DIR ssidk/bifrost_whats_my_species:latest -id $sample._id
+# for IMAGE in bifrost_min_read_check
+for IMAGE in bifrost_min_read_check bifrost_whats_my_species bifrost_assemblatron bifrost_ssi_stamper bifrost_ariba_mlst bifrost_ariba_resfinder bifrost_ariba_virulencefinder bifrost_ariba_plasmidfinder
+do
+    docker run -w $BIFROST_DIR/output/$sample.name --env BIFROST_DB_KEY --mount type=bind,source=$BIFROST_DIR,target=$BIFROST_DIR ssidk/$IMAGE:latest -id $sample._id
+done
 
 cd ..;
--- test_bifrost.sh
+++ test_bifrost.sh
@@ -8,12 +8,23 @@ cd $BIFROST_DIR; \
 docker run \
     --env BIFROST_DB_KEY \
     --mount type=bind,source=$BIFROST_DIR,target=$BIFROST_DIR \
-    ssidk/bifrost_run_launcher:v2.0.9__NA \
+    ssidk/bifrost_run_launcher:latest \
         -pre $BIFROST_DIR/pre.sh \
         -per $BIFROST_DIR/per_sample.sh \
         -post $BIFROST_DIR/post.sh \
         -reads $BIFROST_DIR/read_data \
         -meta $BIFROST_DIR/run_metadata.tsv \
         -name bifrost_test \
-        -out $BIFROST_DIR; 
+        -out $BIFROST_DIR/output; 
+# for IMAGE in bifrost_min_read_check
+for IMAGE in bifrost_min_read_check bifrost_whats_my_species bifrost_assemblatron bifrost_ssi_stamper bifrost_cge_mlst bifrost_cge_resfinder bifrost_ariba_virulencefinder bifrost_ariba_plasmidfinder
+do
+    echo "Downloading and installing $IMAGE"
+    docker run \
+        --env BIFROST_DB_KEY \
+        ssidk/$IMAGE:latest \
+        --install;
+done
+
+cd output;
 bash run_script.sh;
