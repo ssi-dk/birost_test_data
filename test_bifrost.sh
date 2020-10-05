2fadb7c95deca10ad86541894487edc73c212465
Kim Ng <kimleeng@gmail.com>
2020-10-05 08:26:22 +0200

    Update run to allow for env variables

--- test_bifrost.sh
+++ test_bifrost.sh
@@ -1,21 +1,22 @@
 #/bin/bash
 #pass mongodb key as variable
 export BIFROST_DB_KEY="${BIFROST_DB_KEY:-$1}"
-BIFROST_DIR=$PWD; \
+BIFROST_RUN_DIR="${BIFROST_RUN_DIR:-$PWD}"; \
+BIFROST_CONFIG_DIR="${BIFROST_CONFIG_DIR:-$PWD}"; \
 cd read_data; \
 bash download_S1.sh; \
-cd $BIFROST_DIR; \
+cd $BIFROST_RUN_DIR; \
 docker run \
     --env BIFROST_DB_KEY \
-    --mount type=bind,source=$BIFROST_DIR,target=$BIFROST_DIR \
+    --mount type=bind,source=$BIFROST_RUN_DIR,target=$BIFROST_RUN_DIR \
     ssidk/bifrost_run_launcher:latest \
-        -pre $BIFROST_DIR/pre.sh \
-        -per $BIFROST_DIR/per_sample.sh \
-        -post $BIFROST_DIR/post.sh \
-        -reads $BIFROST_DIR/read_data \
-        -meta $BIFROST_DIR/run_metadata.tsv \
+        -pre $BIFROST_CONFIG_DIR/pre.sh \
+        -per $BIFROST_CONFIG_DIR/per_sample.sh \
+        -post $BIFROST_CONFIG_DIR/post.sh \
+        -reads $BIFROST_RUN_DIR/read_data \
+        -meta $BIFROST_RUN_DIR/run_metadata.tsv \
         -name bifrost_test \
-        -out $BIFROST_DIR/output; 
+        -out $BIFROST_RUN_DIR/bifrost_test_output; 
 # for IMAGE in bifrost_min_read_check
 for IMAGE in bifrost_min_read_check bifrost_whats_my_species bifrost_assemblatron bifrost_ssi_stamper bifrost_ariba_mlst bifrost_ariba_resfinder bifrost_ariba_virulencefinder bifrost_ariba_plasmidfinder
 do
