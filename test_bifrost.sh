8f0db4a704dfc6a6b30c880659b60ff0af747d9c
Kim Ng <kimleeng@gmail.com>
2021-02-16 11:41:14 +0100

    Update test_bifrost.sh
    -Update to match RUN dir as that's where it's intended to start from instead of PWD though they're likely he same.
    -name was not unique for purpose of re-testing components though I can see the interest in doing that

--- test_bifrost.sh
+++ test_bifrost.sh
@@ -3,7 +3,7 @@
 export BIFROST_DB_KEY="${BIFROST_DB_KEY:-$1}"
 export BIFROST_RUN_DIR="${BIFROST_RUN_DIR:-$PWD}"; \
 export BIFROST_CONFIG_DIR="${BIFROST_CONFIG_DIR:-$PWD}"; \
-export BIFROST_DIR=`pwd`; \
+export BIFROST_DIR=$BIFROST_RUN_DIR; \
 cd samples; \
 bash download_S1.sh; \
 cd $BIFROST_RUN_DIR; \
@@ -17,7 +17,7 @@ docker run \
         -post $BIFROST_CONFIG_DIR/post.sh \
         -reads $BIFROST_RUN_DIR/samples \
         -meta $BIFROST_RUN_DIR/run_metadata.tsv \
-        -name "`date`" \
+        -name "bifrost_test" \
         -out $BIFROST_RUN_DIR/bifrost_test_output; 
 # for IMAGE in bifrost_min_read_check
 for IMAGE in bifrost_min_read_check bifrost_whats_my_species bifrost_assemblatron bifrost_ssi_stamper bifrost_ariba_mlst bifrost_ariba_resfinder bifrost_ariba_virulencefinder bifrost_ariba_plasmidfinder
